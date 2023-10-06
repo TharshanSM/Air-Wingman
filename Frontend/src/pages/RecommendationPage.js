8cdd35c8d242dfd11a78afe4f6ec3755a6f1e40f
TharshanSM <ugatharshan95@gmail.com>
2023-10-06 22:19:16 +0530

    Modified Layout

---
+++ Frontend/src/pages/RecommendationPage.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+import Footer from "../components/Footer";
+
+const RecommendationPage = () => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    return (
+        <body className={isSidebarOpen ? "toggle-sidebar" : ""}>
+            <header
+                id="header"
+                className="header fixed-top d-flex align-items-center"
+            >
+                <div className="d-flex align-items-center justify-content-between">
+                    <a
+                        href="index.html"
+                        className="logo d-flex align-items-center"
+                    >
+                        <img src="/logo.png" alt="" />
+                        <span className="d-none d-lg-block">Air Wingman</span>
+                    </a>
+                    <i
+                        className="bi bi-list toggle-sidebar-btn"
+                        onClick={() => {
+                            setIsSidebarOpen((isSidebarOpen) => !isSidebarOpen);
+                        }}
+                    />
+                </div>
+                <Navbar></Navbar>
+            </header>
+            <Sidebar active="recommendation"></Sidebar>
+            <main id="main" className="main">
+                <div className="pagetitle">
+                    <h1>Recommendations</h1>
+                    <nav>
+                        <ol className="breadcrumb">
+                            <li className="breadcrumb-item">
+                                <a href="index.html">Home</a>
+                            </li>
+                            <li className="breadcrumb-item active">
+                                Recommendations
+                            </li>
+                        </ol>
+                    </nav>
+                </div>
+            </main>
+            <Footer></Footer>
+        </body>
+    );
+};
+
+export default RecommendationPage;
